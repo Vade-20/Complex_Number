507b91e8c41751a5da105e2ee968803479b3b718
Vade-20 <vedantsaxena2012@gmail.com>
2023-05-20 15:21:45 +0530

    Inital commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# Complex Number Class
+
+This is a Python class that represents complex numbers. It provides various operations such as addition, subtraction, multiplication, and division on complex numbers. The class also includes methods to check if a given string represents a valid complex number and to format the complex number for display.
+
+## Usage
+
+To use the `Complex` class, follow these steps:
+
+1. Import the required modules:
+   ```python
+   import re
+   from fractions import Fraction
+   ```
+
+2. Create an instance of the `Complex` class by passing a string representing a complex number to the constructor:
+   ```python
+   complex_number = Complex("3+4i")
+   ```
+
+3. Perform operations on complex numbers using the available methods. For example:
+   ```python
+   complex_number_1 = Complex("3+4i")
+   complex_number_2 = Complex("2-1i")
+
+   # Addition
+   result = complex_number_1 + complex_number_2
+
+   # Subtraction
+   result = complex_number_1 - complex_number_2
+
+   # Multiplication
+   result = complex_number_1 * complex_number_2
+
+   # Division
+   result = complex_number_1 / complex_number_2
+   ```
+
+4. Display the complex number using the `str` method or by accessing the `complex_number` property:
+   ```python
+   print(complex_number)  # Output: 3+4i
+
+   # Accessing individual components
+   print(complex_number.real)  # Output: 3
+   print(complex_number.imag)  # Output: 4i
+   ```
+
+## Class Methods
+
+The `Complex` class provides the following methods:
+
+- `__init__(self, complex_number: str)`: Initializes a complex number object based on the provided string representation.
+- `iscomplex(self)`: Checks if the complex number is valid.
+- `__add__(self, second)`: Performs addition of two complex numbers.
+- `__sub__(self, second)`: Performs subtraction of two complex numbers.
+- `__mul__(self, second)`: Performs multiplication of two complex numbers.
+- `__truediv__(self, second)`: Performs division of two complex numbers.
+- `__str__(self) -> str`: Returns a string representation of the complex number.
+
+## Properties
+
+The `Complex` class provides the following properties:
+
+- `complex_number`: Returns the formatted string representation of the complex number.
+- `real`: Returns the real component of the complex number as a `Fraction` object.
+- `imag`: Returns the imaginary component of the complex number as a string.
+
+## Example
+
+Here's an example usage of the `Complex` class:
+
+```python
+# Create a complex number object
+complex_number = Complex("3+4i")
+
+# Perform operations
+complex_number_2 = Complex("2-1i")
+result = complex_number + complex_number_2
+
+# Display the result
+print(result)  # Output: 5+3i
+```
+
+Please note that the provided string must represent a valid complex number for the operations to work correctly.
---
+++ complex_number.py
@@ -0,0 +1,119 @@
+import re 
+from fractions import Fraction
+class Complex:
+    def __init__(self,complex_number:str):
+        self.__complex_number = complex_number
+        self.__complex_number.replace('(','')
+        self.__complex_number.replace(')','')
+        assert self.iscomplex(),f'{self.complex_number} is not a proper complex number'
+        real = re.findall(r'[-]?[\d\.//]+(?![i])\b',complex_number)
+        imag = re.findall(r'([-]?[\d\.//]*)([i])',complex_number)
+        real = list(map(lambda x:Complex.__useless(x),real))
+        if len(real)>1:
+            real = list(map(Fraction,real))
+            real = [sum(real)]
+        else:
+            if real==[]:
+                real = [Fraction(0)]
+            else:
+                real[0] = Fraction(real[0])
+        if len(imag)>1:
+            ans = 0
+            for i in imag:
+                if i[0]=='':
+                    ans+=Fraction(1)
+                elif i[0]=='-':
+                    ans+=Fraction(-1)
+                else:
+                    ans+=Fraction(i[0])
+            imag = [int(ans)]
+        else:
+            ans = imag[0][0]
+            if ans=='':
+                imag = [Fraction(1)]
+            elif ans=='-':
+                imag = [Fraction(-1)]
+            else:
+                imag = [Fraction(ans)]
+            
+        self.__real = real[0] if real!=[] else 0
+        self.__imag = imag[0] if imag!=[] else 0
+    
+    def iscomplex(self):
+        for i in self.__complex_number:
+            if not i.isdigit() and i not in ['+','-','i','.','/']:
+                return False
+        else:
+            return True
+        
+    @staticmethod 
+    def __useless(x):
+        if x[len(x)-1]=='/':
+            return 0
+        else:
+            return x
+    
+    @property
+    def complex_number(self):
+        r = self.__real
+        i = self.__imag
+        if i>=0 and i.denominator==1:
+            i = f'+{i}i'
+        elif i<0 and i.denominator==1:
+            i = f'{i}i'
+        elif i>=0:
+            i = f'+({i})i'
+        else:
+            i = f'-({-i})i'
+        return f'{r}{i}'
+    
+    @property
+    def real(self):
+        return Fraction(self.__real) 
+    
+    @property
+    def imag(self):
+        i = self.__imag
+        if i.denominator!=1:
+            i = f'({i})i'
+        else:
+            i = f'{i}i'
+        return i
+    
+    
+    def __add__(self,second):
+        real_new = self.__real+second.__real
+        imag_new = second.__imag+self.__imag
+        imag_new = f'+{imag_new}i' if imag_new>=0 else f'{imag_new}i'
+        return Complex(f'{real_new}{imag_new}')
+    
+    def __sub__(self,second):
+        real_new = self.__real-second.__real
+        imag_new = self.__imag-second.__imag
+        imag_new = f'+{imag_new}i' if imag_new>=0 else f'{imag_new}i'
+        return Complex(f'{real_new}{imag_new}')
+    
+    def __mul__(self,second):
+        x_1,y_1 = self.__real,self.__imag      
+        x_2,y_2 = second.__real,second.__imag
+        real_new = (x_1*x_2)-(y_1*y_2)
+        imag_new = (x_1*y_2)+(x_2*y_1)
+        imag_new = f'+{imag_new}' if imag_new>=0 else imag_new
+        return Complex(f'{real_new}{imag_new}i')
+    
+    def __truediv__(self,second):
+        x_1,y_1 = self.__real,self.__imag
+        x_2,y_2 = second.__real,second.__imag
+        real_new =Fraction(((x_1*x_2)+(y_1*y_2))/((x_2**2)+(y_2**2)))
+        imag_new =-Fraction(((x_1*y_2)-(y_1*x_2))/((x_2**2)+(y_2**2)))
+        imag_new = f'+{imag_new}i' if imag_new>=0 else f'{imag_new}i'
+        return Complex(f'{real_new}{imag_new}')
+        
+        
+    def __str__(self) -> str:
+        if self.iscomplex():
+            return self.complex_number
+        else:
+            raise Exception ("Please enter a complex number")
+        
+
